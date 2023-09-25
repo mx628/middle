f12a406f2fe586400868ee5108bbb2e23f9bab02
Boris Tryukhan <143996710+mx628@users.noreply.github.com>
2023-09-25 22:06:55 +0300

    itc_16-18.cpp

--- itc_16-18.cpp
+++ itc_16-18.cpp
@@ -1,19 +1,5 @@
-#include <iostream>
-#include "middle.h"
-
-int itc_rev_oct_num(long long number){
-    int dec = 0;
-    int step = 0;
-    while(number > 0){
-        dec += number % 10 * itc_pow(8, step);
-        number/=10;
-        ++step;
-    }
-    return dec;
-}
-
 int itc_covert_num(long long number, int ss){
-    int binare = 0, n = 1;
+    long long binare = 0, n = 1;
     while (number > 0){
         binare += (number % ss) * n;
         n*=10;
@@ -23,7 +9,7 @@ int itc_covert_num(long long number, int ss){
 }
 
 int itc_rev_covert_num(long long number, int ss){
-    int dec = 0;
+    long long dec = 0;
     int step = 0;
     while(number > 0){
         dec += number % 10 * itc_pow(ss, step);
