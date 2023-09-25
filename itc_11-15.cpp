ba6c8fb4d7f2b6ce71957b5bc762d514394fa6cb
Boris Tryukhan <143996710+mx628@users.noreply.github.com>
2023-09-25 20:49:55 +0300

    itc_11-15.cpp

--- itc_11-15.cpp
+++ itc_11-15.cpp
@@ -12,7 +12,7 @@ int itc_second_max_num(long long number) {
             max2 = max1;
             max1 = n;
         }
-        else if (n > max2 && n < max1) {
+        else if (n > max2 && n <= max1) {
             max2 = n;
         }
         number /= 10;
@@ -22,28 +22,13 @@ int itc_second_max_num(long long number) {
 
 
 int itc_second_simple_max_num(long long number) {
-    int max1 = -1, max2 = -1;
-    if(number < 10)
+    if (itc_second_max_num(number) == itc_max_num(number))
         return -1;
-        int n;
-    while (number > 0) {
-        n = number % 10;
-        if (n > max1) {
-            max2 = max1;
-            max1 = n;
-        }
-        else if (n > max2 && n <= max1) {
-            max2 = n;
-        }
-        number /= 10;
-    }
-    if(max1 == max2)
-        return -1;
-    return max2;
+    return itc_second_max_num(number);
 }
 
 long long itc_bin_num(long long number){
-    int binare = 0, n = 1;
+    long long binare = 0, n = 1;
     while (number > 0){
         binare += (number % 2) * n;
         n*=10;
@@ -53,7 +38,7 @@ long long itc_bin_num(long long number){
 }
 
 long long itc_oct_num(long long number){
-    int binare = 0, n = 1;
+    long long binare = 0, n = 1;
     while (number > 0){
         binare += (number % 8) * n;
         n*=10;
